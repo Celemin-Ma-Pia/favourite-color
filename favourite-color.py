619a40a64e17856ce3e87e787b2c45eabb496787
Ma. Pia Paula Celemin <116002019+Celemin-Ma-Pia@users.noreply.github.com>
2022-10-28 12:08:51 +0800

    Add files via upload

---
+++ favourite-color.py
@@ -0,0 +1,5 @@
+color = input ("Type in your favorite color: ")
+if color == "red" or color == "RED" or color == "Red":
+    print("I like red too.")
+else:
+    print("I don't like that color, I prefer red")
